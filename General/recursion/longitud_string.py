89986f659355faa08f824e07534113a58fe14b12
John <jyaxp1@miumg.edu.gt>
2024-10-02 21:11:54 -0600

    Change, longitud string

--- General/recursion/longitud_string.py
+++ General/recursion/longitud_string.py
@@ -2,9 +2,20 @@
 
 palabra = 'PasteldeFresa'
 
+# Metodo iterativo
+
+def string_len_iter(input_str):
+    contador = 0
+    for i in input_str:
+        contador += 1
+    return contador
+
+print(f'Método iterativo: {string_len_iter(palabra)}')
+
+# Metodo recursivo
 def string_len(input_str):
     if input_str == '':
         return 0
     return 1 + string_len(input_str[1:])
         
-print(string_len(palabra))
+print(f'Método recursivo: {string_len(palabra)}')
